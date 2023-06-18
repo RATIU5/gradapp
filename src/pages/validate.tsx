bf91aa4cc4b834f9968c37a9da75ad5f00c37e63
RATIU5 <jcmemmott20@outlook.com>
2023-06-17 18:58:21 -0600

    add: basic validate form

--- src/pages/validate.tsx
+++ src/pages/validate.tsx
@@ -1,10 +1,32 @@
 import { type NextPage } from "next";
+import { useState } from "react";
 import Layout from "~/components/layout";
 
 type Validate = object;
 
 const Validate: NextPage<Validate> = (props) => {
-  return <Layout></Layout>;
+  const [nameValue, setNameValue] = useState("");
+
+  const onChange = (value: string) => {
+    setNameValue(value);
+    // searchGuests(query);
+  };
+
+  return (
+    <Layout>
+      <div>
+        <form>
+          <input
+            type="text"
+            name="attendee_name"
+            id="attendee_name"
+            value={nameValue}
+            onChange={(e) => onChange(e.target.value as string)}
+          />
+        </form>
+      </div>
+    </Layout>
+  );
 };
 
 export default Validate;
