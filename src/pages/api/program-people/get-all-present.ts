3369c5ecb9a6a464582439bd7d37fc3ae856129a
RATIU5 <jcmemmott20@outlook.com>
2023-06-22 09:41:47 -0600

    add: arrival query

---
+++ src/pages/api/program-people/get-all-present.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import supabase from "~/server/db";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { data: arrivals, error } = await supabase
+    .from("programs")
+    .select("*, people ( * )")
+    .eq("people.present", true);
+  if (error) {
+    return res.status(500).json({ error });
+  }
+  return res.status(200).json({ data: arrivals });
+}
--- src/pages/arrivals.tsx
+++ src/pages/arrivals.tsx
@@ -1,24 +1,32 @@
 import { type NextPage } from "next";
 import { useQuery } from "@tanstack/react-query";
 import Layout from "~/components/layout";
+import { getAllPresentPeopleWithPrograms } from "~/utils/query-fns";
+import QueryData from "~/components/layout/query-data";
 
 type Arrivals = object;
 
 const Arrivals: NextPage<Arrivals> = (props) => {
   const attendeesQuery = useQuery({
     queryKey: ["getAttendees"],
-    queryFn: getAttendees,
-  });
-  const graduatesQuery = useQuery({
-    queryKey: ["getGraduates"],
-    queryFn: getGraduates,
-  });
-  const departmentQuery = useQuery({
-    queryKey: ["getDepartments"],
-    queryFn: getDepartments,
+    queryFn: getAllPresentPeopleWithPrograms,
   });
 
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <QueryData dataQuery={attendeesQuery}>
+        {(attendeesArray) => {
+          return (
+            <>
+              {attendeesArray.map((r) => (
+                <div></div>
+              ))}
+            </>
+          );
+        }}
+      </QueryData>
+    </Layout>
+  );
 };
 
 export default Arrivals;
