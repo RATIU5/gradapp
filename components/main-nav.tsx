627f77683527c1f1931b4fa2219c3e5c9ddd04a3
RATIU5 <jcmemmott20@outlook.com>
2023-07-14 07:24:28 -0600

    edit: nav links; add: admin page

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -9,32 +9,31 @@ const MainNav = async (props: MainNavProps) => {
   return (
     <div className="w-full flex justify-center">
       <div className="flex fixed w-full justify-between items-center max-w-3xl rounded-full bg-neutral-100/50 px-4 py-1 text-neutral-600 translate-y-5 dark:bg-neutral-800/50 dark:text-neutral-400 border-solid border border-neutral-200 dark:border-neutral-700 backdrop-blur-md ">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="w-12 h-12"
-        >
-          <path d="M22 10v6M2 10l10-5 10 5-10 5z" />
-          <path d="M6 12v5c3 3 9 3 12 0v-5" />
-        </svg>
+        <a href="/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="w-12 h-12"
+          >
+            <path d="M22 10v6M2 10l10-5 10 5-10 5z" />
+            <path d="M6 12v5c3 3 9 3 12 0v-5" />
+          </svg>
+        </a>
         <ul className="flex justify-between items-center">
-          <li className="mx-2 dark:hover:text-neutral-50 text-neutral-500 hover:text-neutral-950">
-            <a href="/">Attendees</a>
-          </li>
           {session && (
             <>
               <li className="mx-2 dark:hover:text-neutral-50 text-neutral-500 hover:text-neutral-950">
-                <a href="/people">People</a>
+                <a href="/attendees">Attendees</a>
               </li>
               <li className="mx-2 dark:hover:text-neutral-50 text-neutral-500 hover:text-neutral-950">
-                <a href="/programs">Programs</a>
+                <a href="/people">Admin</a>
               </li>
             </>
           )}
