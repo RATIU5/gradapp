74160b973dc650d2ce0c0ad1d4d5be4f34eef066
RATIU5 <jcmemmott20@outlook.com>
2023-07-16 17:34:12 -0600

    add: formatting to upload box

--- components/upload-students.tsx
+++ components/upload-students.tsx
@@ -4,13 +4,18 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { addNewStudents$ } from "@/lib/queries";
+import { Icons } from "./icons";
 
 const UploadStudents = () => {
   const queryClient = useQueryClient();
   const [file, setFile] = useState<File | undefined>(undefined);
   const newStudentMutation = useMutation({
     mutationFn: (peopleCSV: string) => addNewStudents$(peopleCSV),
+    onError: (error: any) => {
+      console.error(error);
+    },
     onSuccess: async () => {
+      setFile(undefined);
       await queryClient.refetchQueries({ stale: true });
     },
   });
@@ -39,14 +44,20 @@ const UploadStudents = () => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        type="file"
-        name="file"
-        onChange={(e) => setFile(e.target.files?.[0])}
-      />
-      <Button type="submit" disabled={!file}>
-        Import Students
+    <form onSubmit={onSubmit} className="flex flex-col items-stretch">
+      <div className="w-full border-2 border-neutral-200 border-dashed rounded-lg p-4">
+        <input
+          type="file"
+          name="file"
+          onChange={(e) => setFile(e.target.files?.[0])}
+        />
+      </div>
+      <Button type="submit" disabled={!file || newStudentMutation.isLoading}>
+        {newStudentMutation.isLoading ? (
+          <Icons.spinner className="animate-spin" />
+        ) : (
+          "Import Students"
+        )}
       </Button>
     </form>
   );
