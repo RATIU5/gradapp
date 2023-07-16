4e2db610e99e51d0ffcd6a60e17f29eff250002c
RATIU5 <jcmemmott20@outlook.com>
2023-07-16 16:29:12 -0600

    add: formatting objedt data for add route

--- app/api/db/people/route.ts
+++ app/api/db/people/route.ts
@@ -1,19 +1,26 @@
 import supabase from "@/lib/db";
-import { Person } from "@/lib/types";
+import { Student } from "@/lib/types";
 import { NextResponse } from "next/server";
 
-export const POST = async (
-  req: Request,
-  { params }: { params: { person: Person } }
-) => {
-  console.log(params);
+export const POST = async (req: Request) => {
+  const body = (await req.json()) as Student[];
+  const formattedObjects = body.map((person: Student) => {
+    return {
+      firstname: person.firstname,
+      lastname: person.lastname,
+      programid: person.programid,
+      email: person.email,
+      platinum: person.platinum,
+      highschool: person.highschool,
+      persontype: person.studentfaculty ? 3 : 2,
+      present: false,
+    };
+  });
 
-  // const body = await req.json();
-  // const { data, error } = await supabase
-  //   .from("people")
-  //   .update({ present: body.present })
-  //   .eq("id", params.id)
-  //   .select();
-  // if (error) return NextResponse.json({ data: error }, { status: 500 });
-  // return NextResponse.json({ data }, { status: 200 });
+  const { data, error } = await supabase
+    .from("people")
+    .insert(formattedObjects)
+    .select();
+  if (error) return NextResponse.json({ data: error }, { status: 500 });
+  return NextResponse.json({ data }, { status: 200 });
 };
