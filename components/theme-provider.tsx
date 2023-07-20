39f7e39d9625173009cdb1aea0c15610c1ba4149
RATIU5 <jcmemmott20@outlook.com>
2023-07-20 17:01:58 -0600

    init svelte app

--- components/theme-provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-// Client-side only here instead of global layout
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-}
--- lib/db.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.SUPABASE_URL as string;
-const supabaseKey = process.env.SUPABASE_PUBLIC_KEY as string;
-const supabaseOptions = {};
-const supabase = createClient(supabaseUrl, supabaseKey, supabaseOptions);
-
-export default supabase;
