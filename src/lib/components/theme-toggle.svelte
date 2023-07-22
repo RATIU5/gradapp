6dbf569c39fcb70d720ff92c985a586a6ab19842
RATIU5 <jcmemmott20@outlook.com>
2023-07-22 09:11:09 -0600

    core: removed melt, installed svelteui

--- src/lib/components/theme-toggle.svelte
+++ src/lib/components/theme-toggle.svelte
@@ -1,12 +1,28 @@
 <script>
 	import { createDropdownMenu } from '@melt-ui/svelte';
+	import { Moon, Settings, Sun } from 'lucide-svelte';
 	const { menu, item, trigger, arrow } = createDropdownMenu();
 </script>
 
-<button melt={$trigger}>Click me</button>
+<button melt={$trigger} class="p-2 hover:bg-black/5 rounded-lg text-neutral-800"><Sun /></button>
 <div melt={$menu}>
-	<div {...$item} use:item>...</div>
-	<div {...$item} use:item>...</div>
-	<div {...$item} use:item>...</div>
+	<div melt={$item}>
+		<div>
+			<Sun />
+			<span>Light</span>
+		</div>
+	</div>
+	<div melt={$item}>
+		<div>
+			<Moon />
+			<span>Dark</span>
+		</div>
+	</div>
+	<div melt={$item}>
+		<div>
+			<Settings />
+			<span>System</span>
+		</div>
+	</div>
 	<div melt={$arrow} />
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,17 @@
 <script>
 	import '../app.css';
 	import Header from '$lib/components/header.svelte';
+	import { SvelteUIProvider, colorScheme } from '@svelteuidev/core';
 </script>
 
-<div class="flex min-h-screen flex-col overflow-hidden">
-	<header>
-		<Header />
-	</header>
-	<main class="flex-grow">
-		<slot />
-	</main>
-	<footer>Footer</footer>
-</div>
+<SvelteUIProvider themeObserver={$colorScheme}>
+	<div class="flex min-h-screen flex-col overflow-hidden">
+		<header>
+			<Header />
+		</header>
+		<main class="flex-grow">
+			<slot />
+		</main>
+		<footer>Footer</footer>
+	</div>
+</SvelteUIProvider>
