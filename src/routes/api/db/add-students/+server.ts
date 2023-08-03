f0f1a400a4890444a80ac5b85b649d15e642afa7
RATIU5 <jcmemmott20@outlook.com>
2023-08-03 08:55:58 -0600

    add: add student api route

--- src/routes/api/db/add-students/+server.ts
+++ src/routes/api/db/add-students/+server.ts
@@ -1,7 +1,40 @@
-import type { RequestHandler } from "@sveltejs/kit";
+import supabase from "$lib/server/db.server";
+import { json, type RequestHandler } from "@sveltejs/kit";
 
-export const POST: RequestHandler = async ({ request }) => {o
+export type Input = {
+    firstname: string;
+    lastname: string;
+    email: string;
+    programid: number;
+    platinum: boolean;
+    highschool: boolean;
+    isfaculty: boolean;
+}[];
+export const POST = (async ({ request }) => {
 
-   const body = (await request.json());
+    try {
+        const body = (await request.json());
+        
+        for (let i = 0; i < body.length; i++) {
+            if (body[i].isfaculty) {
+                body[i].persontype = 3;
+            } else {
+                body[i].persontype = 2;
+            }
+            delete body[i].isfaculty;
+            body[i].present = false;
+        }
 
-}
+        const { data, error } = await supabase.from("people").insert(body).select();
+
+        if (error) {
+            throw new Error(error.message);
+        }
+        
+        return json(data);
+
+    } catch(e: any) {
+        return json({error: e.message});
+    }
+     
+}) satisfies RequestHandler;
