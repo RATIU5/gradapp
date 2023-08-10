1dfa20188efaab3db91e69e4de92ce40794c58cc
RATIU5 <jcmemmott20@outlook.com>
2023-08-09 18:31:31 -0600

    edit: menu

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,26 +1,17 @@
 <script lang="ts">
-	import { GraduationCap, Menu } from 'lucide-svelte';
-	import ThemeToggle from './theme-toggle.svelte';
+	import { Menu } from 'lucide-svelte';
+	import Navbar from './navbar.svelte';
 
-    
+	let isMenuOpen = false;
+
+	function toggleMenu() {
+		isMenuOpen = !isMenuOpen;
+	}
 </script>
 
-
-<div>
-    <Menu />
-</div>
-
-<nav
-	class="flex justify-between items-center my-4 rounded-full py-2 px-4 w-full max-w-4xl bg-white/50 backdrop-blur-lg border border-neutral-200 border-solid"
->
-	<a href="/">
-		<GraduationCap strokeWidth={1.75} size={32} class="text-neutral-500" />
-	</a>
-	<ul class="flex">
-		<li class="mx-2 text-md text-neutral-600"><a href="/">List</a></li>
-		<li class="mx-2 text-md text-neutral-600"><a href="/login">Login</a></li>
-	</ul>
-	<div>
-		<ThemeToggle />
-	</div>
-</nav>
+<button on:click={toggleMenu}>
+	<Menu />
+</button>
+{#if isMenuOpen}
+	<Navbar />
+{/if}
---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Home, GraduationCap } from 'lucide-svelte';
+</script>
+
+<nav class="flex-grow flex fixed bottom-0 w-full">
+	<ul class="flex w-full justify-around py-4">
+		<li>
+			<a href="/" class="p-2"><Home /></a>
+		</li>
+		<li>
+			<a href="/graduates" class="p-2"><GraduationCap /></a>
+		</li>
+		<li>
+			<a href="/login" class="p-2"><GraduationCap /></a>
+		</li>
+	</ul>
+</nav>
