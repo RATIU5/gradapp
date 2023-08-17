a2856321835e2380c25851bb92dc81661c9a797b
RATIU5 <jcmemmott20@outlook.com>
2023-08-17 08:37:18 -0600

    fix: filtering

--- src/routes/attendees/+page.svelte
+++ src/routes/attendees/+page.svelte
@@ -1,28 +1,30 @@
 <script lang="ts">
 	import AttendeeRow from '$lib/components/attendee-row.svelte';
+	import type { AllAttendeesData } from '$lib/utils/types';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-    
-    let value: string;
 
-    function filter(e: InputEvent & { target: HTMLInputElement }) {
-        if (e && e.target.value !== "") {
-          const value = e.target.value;
-          for (let i = 0; i < data.attendees.length; i++) {
-              return data.attendees.filter((student) => {
-                 const res = [];
-                 const fullName = `${student.firstname} ${student.lastname}`;
-                 if (fullName.includes(value)) {
-                   res.push(student)
-                 } else if (student.programs.name.includes(value)) {
-                   res.push(student);
-                 }
-              });
-          }
-        }
-        return data.attendees;
-    }
+	let value = '';
+	let filteredAttendees: AllAttendeesData[] = data.attendees;
+
+	$: {
+		if (value !== '') {
+			const res: AllAttendeesData[] = [];
+			for (let i = 0; i < data.attendees.length; i++) {
+				const student = data.attendees[i];
+				const fullName = `${student.firstname} ${student.lastname}`;
+				if (fullName.includes(value)) {
+					res.push(student);
+				} else if (student.programs.name.includes(value)) {
+					res.push(student);
+				}
+			}
+			filteredAttendees = res;
+		} else {
+			filteredAttendees = data.attendees;
+		}
+	}
 </script>
 
 <div class="w-full pb-12 relative lg:pt-20">
@@ -40,15 +42,14 @@
 			class="fixed bottom-16 lg:bottom-auto lg:top-16 lg:mt-2 left-1/2 -translate-x-1/2 z-40 w-full px-4 max-w-2xl"
 		>
 			<input
-                bind:value={value}
-                on:input={filter}
+				bind:value
 				type="text"
 				class="w-full border rounded-full px-4 py-2 mb-4 shadow-md"
 				placeholder="Search"
 			/>
 		</div>
 		<div class="mt-12 lg:mt-0">
-			{#each filter() as attendee}
+			{#each filteredAttendees as attendee}
 				<AttendeeRow {attendee} />
 			{/each}
 		</div>
