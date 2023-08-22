03c022cbd7057b661c57346490ef31a69f87c49f
RATIU5 <jcmemmott20@outlook.com>
2023-08-22 16:56:05 -0600

    add: partial of tabs on admin page

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,4 +1,11 @@
-<script lang="ts">
+<script>
+	import Tabs from '$lib/components/ui/tabs.svelte';
+
+	let items = [
+		{ label: 'Content', value: 1, component: Tab1 },
+		{ label: 'Interactions', value: 2, component: Tab2 },
+		{ label: 'Tab 3', value: 3, component: Tab3 }
+	];
 </script>
 
-<div />
+<Tabs {items} />
